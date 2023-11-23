7c32f92de6d5c58b8574487cba3b9f9aa008c76a
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-11-23 21:27:26 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
+import { useState } from 'react';
+import { useGetContactsQuery, useAddContactMutation, useDeleteContactMutation } from '../redux';
+import ContactForm from './ContactForm/ContactForm'
+function App() {
+  
+  const [newContact, setNewContact] = useState('');
+  const {data = [], isLoading} = useGetContactsQuery();
+  const [addContact] = useAddContactMutation();
+  const [deleteContact] = useDeleteContactMutation();
+
+  const handleAddContact = async (contact) => {
+    if(contact) {
+      setNewContact(contact)
+       await addContact(newContact).unwrap();
+     
+       setNewContact('');
+    }
+  }
+
+  const handleDeleteContact = async (id) => {
+    await deleteContact(id).unwrap();
+  }
+
+  if (isLoading) return <h1>Loading...</h1>
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {/* <div>
+        <input
+          type="text"
+          value={newContact}
+          onChange={(e) => setNewContact(e.target.value)}
+        />
+        <button onClick={handleAddContact}>Add contact</button>
+      </div> */}
+       <ContactForm onSubmit={handleAddContact} />
+      <div>
+       
+      </div>
+      <ul>
+        {data.map(item => (
+          <li key={item.id} onClick={() => handleDeleteContact(item.id)}>
+            {item.name} {item.phone}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
+
+export default App;
